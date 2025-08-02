36fe9a0f84fed0caf026a25933b565cbf2b87132
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2025-08-02 22:53:22 +1000

    Chain together prod login scripts, add Jira

--- dot_dotfiles/work/work.zsh
+++ dot_dotfiles/work/work.zsh
@@ -15,3 +15,19 @@ fi
 if [ -f "$HOME/projects/inf-scripts/eks-jumpbox-session/eks-jumpbox-session" ] ; then
         export PATH="$HOME/projects/inf-scripts/eks-jumpbox-session:$PATH"
 fi
+
+# ================================
+# Aliases
+# ================================
+alias prod-login="switch-env prd-lw && mac-focus-app iTerm2 && eks-jumpbox-session"
+alias prod="prod-assign && prod-login"
+
+# ================================
+# Functions
+# ================================
+# https://github.com/ZimbiX/dotfiles/blob/master/mable/aliases.zsh
+jira() {
+  local jira_issue_number
+  jira_issue_number="$(git rev-parse --abbrev-ref HEAD | grep -oE 'ES-\d+')"
+  open "https://attainhealthtech.atlassian.net/browse/$jira_issue_number"
+}
