019b7c98be5504a56ca02cac39fcbd0443f79a88
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2026-01-07 16:34:20 +1100

    Disable husky, set up Android Studio

--- dot_dotfiles/work/init.zsh
+++ dot_dotfiles/work/init.zsh
@@ -2,6 +2,19 @@
 # Environment Variables
 # ================================
 
+# Disable git hooks
+export WORKUP=0
+
+# Set up Android Studio
+if [ -z ${ANDROID_HOME+x} ]; then
+  export ANDROID_HOME=$HOME/Library/Android/sdk
+  export PATH=$ANDROID_HOME/emulator:$PATH
+  export PATH=$ANDROID_HOME/tools:$PATH
+  export PATH=$ANDROID_HOME/tools/bin:$PATH
+  export PATH=$ANDROID_HOME/platform-tools:$PATH
+  export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
+fi
+
 # ================================
 # In-House Scripts
 # ================================
