2144c24826bf5d3fba38d37f27b812f1067ad213
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2026-07-02 17:34:55 +1000

    Make it so much less annoying to use zellij

--- dot_dotfiles/setup/init.zsh
+++ dot_dotfiles/setup/init.zsh
@@ -1,3 +1,28 @@
+export HISTFILE=~/.zsh_history
+export HISTSIZE=100000
+export SAVEHIST=100000
+
+# Write to the history file immediately, not when the shell exits
+setopt INC_APPEND_HISTORY
+
+# Share history across all concurrent sessions (needed for multiplexers)
+setopt SHARE_HISTORY
+
+# Append to history file
+setopt APPEND_HISTORY
+
+# Save timestamp and duration of commands
+setopt EXTENDED_HISTORY
+
+# Do not record an event that was just recorded again
+setopt HIST_IGNORE_DUPS
+
+# Remove older duplicate entries from history
+setopt HIST_IGNORE_ALL_DUPS
+
+# Do not display a duplicate previously found in search
+setopt HIST_FIND_NO_DUPS
+
 # fzf
 source <(fzf --zsh)
 
